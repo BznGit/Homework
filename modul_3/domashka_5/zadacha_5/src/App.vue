971b92d3a6fe70cab778e2dac4bfd71f115ec7e0
BznGit <73360255+BznGit@users.noreply.github.com>
2021-01-21 22:14:40 +0300

    5*

---
+++ modul_3/domashka_5/zadacha_5/src/App.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="head">
+			<input type="text" class="edit" placeholder="Введите текст для поиска" v-model="search">
+			<button v-on:click="isActive=!isActive">{{isActive?'Список':'Таблица'}}</button>
+		</div>
+		<div class="list" :class={grid:isActive} v-for="article in articles" :key="article">
+				<div class="img">
+					<a v-bind:href="article.url"><img v-bind:src="article.image"></a>
+				</div>
+				<div v-show="!isActive" class="descr">
+					<h3><a v-bind:href="article.url">{{article.titel}}</a></h3>
+					<p>{{article.description}}</p>
+				</div>
+		</div>
+</template>
+
+<script>
+
+
+export default {
+  data() { //используемые данные
+		return {
+			articles:[
+				{
+					titel: "Во что играл британский математик Джон Конвей",
+					url: "https://zen.yandex.ru/media/nplus1/vo-chto-igral-britanskii-matematik-djon-konvei-5e994b703f96c7546c7a28a3",
+					image: "https://avatars.mds.yandex.net/get-zen_doc/198554/pub_5e994b703f96c7546c7a28a3_5e994b8fc0181e4d79c352a4/scale_1200",
+					description:`С кем "играл" Конвей и каким запомнился коллегам, рассказывают математики Виктор Клепцин и Михаил Раскин`
+				},
+				{
+					titel: "Теория игр. Искусство стратегического мышления в бизнесе и жизни",
+					url: "https://zen.yandex.ru/media/id/5a36b95179885ecd8830b12e/teoriia-igr-iskusstvo-strategicheskogo-myshleniia-v-biznese-i-jizni-5a36bde95f4967207c88521a",
+					image: "https://avatars.mds.yandex.net/get-zen_doc/42056/pub_5a36bde95f4967207c88521a_5a36be497ddde8d17c5ca754/scale_1200" ,
+					description:`Стратегия игры: как найти оптимальное решение и убедить соперников, что вы игрете всерьез.`
+				},
+				{
+					titel: "Куда приводит антропный принцип",
+					url: "https://zen.yandex.ru/media/id/5a6df3dc3c50f7f6447b3420/kuda-privodit-antropnyi-princip-5d835fc134808200ad2fc625",
+					image: "https://avatars.mds.yandex.net/get-zen_doc/1889495/pub_5d835fc134808200ad2fc625_5d8360acdf944400aec16adb/scale_1200",
+					description: `Времени не существует.Именно так я хотел начать статью о том, как время можно вывести из антропного принципа.`
+				},
+			],
+			isActive: false,
+			
+		}
+	},
+}
+</script>
+
+<style>
+.head {
+				display: flex;
+				flex-direction: row;
+			}
+
+			.edit {
+				width: 30%;
+				margin-left: 250px;
+				height: 30px;
+				font-size: 25px;
+			}
+
+			button {
+				right: 0;
+				height: 36px;
+				width: 80px;
+				margin-left: 30%;
+				padding: 0;
+			}
+
+
+
+			.list {
+				display: flex;
+
+				border-bottom: 1px solid rgb(70, 6, 219);
+				border-radius: 30px;
+				width: 100%;
+				padding: 2px;
+				font-family: Tahoma;
+				margin: auto;
+				margin-top: 10px;
+				justify-content: stretch;
+			}
+
+			.grid {
+				display: inline-block;
+				border: 1px solid rgb(70, 6, 219);
+
+				width: 250px;
+				margin: 20px;
+
+
+			}
+
+			img {
+				width: 200px;
+				margin: 20px;
+				border-radius: 10px;
+			}
+
+			.img {
+				display: flex;
+				justify-content: center;
+				width: 250px;
+
+			}
+
+			.descr {
+				display: flex;
+				flex-direction: column;
+
+			}
+
+			p {
+				margin: 0;
+				padding: 0;
+			}
+</style>
