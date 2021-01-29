b81a5d058d0183793df3a411250837205a2cb2b1
BznGit <73360255+BznGit@users.noreply.github.com>
2021-01-29 22:17:51 +0300

    так?

--- modul_4/domashka_1/laba/name.js
+++ modul_4/domashka_1/laba/name.js
@@ -4,39 +4,34 @@ const server = http.createServer((request, response)=>{
     console.log(`Запрошенный адрес: ${request.url}`)
     //const filename = request.url.substring(1);
     response.writeHead(200, {'Content-Type':'text/html'});
-     fs.readFile('body.html', 'utf-8', (err, data) => {
+     fs.readFile('body.html', 'utf-8', (err, data1) => {
         if (err) {
             console.log('Could not find or open file for reading\n');
-          //  response.statusCode = 404;
-         //   response.end();
+            response.statusCode = 404;
+            response.end();
         } else {
-            console.log(`The file 'body.html' is read and sent to the client\n`);
-           // response.writeHead(200, {'Content-Type':'text/html'});
-            response.write(data);
-            
+            console.log(`The file 'body.html' is read and sent to the client\n`);       
         };
     
-        fs.readFile('footer.html', 'utf-8', (err, data) => {
+        fs.readFile('footer.html', 'utf-8', (err, data2) => {
             if (err) {
                 console.log('Could not find or open file for reading\n');
-            // response.statusCode = 404;
-            //  response.end();
+                response.statusCode = 404;
+                response.end();
             } else {
                 console.log(`The file 'footer.html' is read and sent to the client\n`);
-            // response.writeHead(200, {'Content-Type':'text/html'});
-                response.write(data);
-                data2=true;
             };
         
-            fs.readFile('header.html', 'utf-8', (err, data) => {
+            fs.readFile('header.html', 'utf-8', (err, data3) => {
                 if (err) {
                     console.log('Could not find or open file for reading\n');
-                //  response.statusCode = 404;
-                //  response.end();
+                    response.statusCode = 404;
+                    response.end();
                 } else {
                     console.log(`The file 'header.html' is read and sent to the client\n`);
-                //  response.writeHead(200, {'Content-Type':'text/html'});
-                    response.write(data);
+                    response.write(data1);
+                    response.write(data2);
+                    response.write(data3);
                     response.end();    
                     console.log("Request accepted!");
                 }
